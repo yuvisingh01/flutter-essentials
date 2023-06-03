fdef44ad7019088d1bf892b0ad526cf8bf6c1d0c
yuvisingh01 <yuvsingh2018@gmail.com>
2023-06-03 15:04:11 +0530

    chapter 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,47 +11,93 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var myText="Change My Name";
+  TextEditingController _nameController=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
       body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          alignment: Alignment.center,
-          // clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.circular(10),
-            gradient: LinearGradient(colors: [
-                  Colors.pink,
-                  Colors.yellow,
-                ]),
-            boxShadow:[
-              BoxShadow(
-                color: Colors.grey,
-                // blurRadius: 10,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                   'assets/images/coding.jpg',
+                  ),
+                  SizedBox(
+                      height: 20
+                  ),
+                  Text(
+                    myText,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextField(
+                    controller: _nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+
+                        ),
+                        hintText: "Enter your text",
+                        labelText: "Name",
+                      ),
+                  ),
+                ],
               ),
-            ]),
-          child: Text(
-            'I am a box',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
             ),
           ),
         ),
       ),
+      // body: Center(
+      //   child: Container(
+      //     padding: const EdgeInsets.all(8),
+      //     width: 100,
+      //     height: 100,
+      //     alignment: Alignment.center,
+      //     // clipBehavior: Clip.antiAlias,
+      //     decoration: BoxDecoration(
+      //       color: Colors.red,
+      //       shape: BoxShape.rectangle,
+      //       borderRadius: BorderRadius.circular(10),
+      //       gradient: LinearGradient(colors: [
+      //             Colors.pink,
+      //             Colors.yellow,
+      //           ]),
+      //       boxShadow:[
+      //         BoxShadow(
+      //           color: Colors.grey,
+      //           // blurRadius: 10,
+      //         ),
+      //       ]),
+      //     child: Text(
+      //       'I am a box',
+      //       textAlign: TextAlign.center,
+      //       style: TextStyle(
+      //           fontSize: 20,
+      //           color: Colors.white,
+      //           fontWeight: FontWeight.bold,
+      //       ),
+      //     ),
+      //   ),
+      // ),
       drawer: Drawer(
         child: ListView(
           padding: const EdgeInsets.all(8),
@@ -84,8 +130,13 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(Icons.edit),
+        onPressed: (){
+          myText=_nameController.text;
+          setState(() {
+
+          });
+        },
+        child: Icon(Icons.send),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
